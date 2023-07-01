b38381c27c6f929614d6b49dd40ec0890d491329
aatish <aatishsingh998@gmail.com>
2023-07-01 18:45:25 +0530

    flatnenning of random user using withcolumn and select and explode

--- sparkproject1/src/main/scala/pack/project.scala
+++ sparkproject1/src/main/scala/pack/project.scala
@@ -38,6 +38,63 @@ object project {
     
     println("test")
     
+    val sslContext = SSLContext.getInstance("TLS")
+					sslContext.init(null, Array(new X509TrustManager {
+						override def getAcceptedIssuers: Array[X509Certificate] = Array.empty[X509Certificate]
+								override def checkClientTrusted(x509Certificates: Array[X509Certificate], s: String): Unit = {}
+					override def checkServerTrusted(x509Certificates: Array[X509Certificate], s: String): Unit = {}
+					}), new java.security.SecureRandom())
+					val hostnameVerifier = new HostnameVerifier {override def verify(s: String, sslSession: SSLSession): Boolean = true}
+
+			val httpClient = HttpClients.custom().setSSLContext(sslContext).setSSLHostnameVerifier(hostnameVerifier).build()
+					val content = EntityUtils.toString(httpClient.execute(new HttpGet("https://randomuser.me/api/0.8/?results=10")).getEntity)
+
+
+					val urlstring = content.mkString
+					println(urlstring)
+			
+			val urlstring = content.mkString
+    println(urlstring)
+    
+    val df =spark.read.json(sc.parallelize(List(urlstring)))
+    
+    df.printSchema()
+    
+    val flatdf = df.withColumn("results", explode(col("results")))
+                   .select(
+                      "nationality",
+                      "seed", 
+                      "version", 
+                      "results.user.cell", 
+                      "results.user.email", 
+                      "results.user.gender", 
+                      "results.user.location.city", 
+                      "results.user.location.state", 
+                      "results.user.location.street", 
+                      "results.user.location.zip", 
+                      "results.user.md5", 
+                      "results.user.name.first", 
+                      "results.user.name.last", 
+                      "results.user.name.title", 
+                      "results.user.password", 
+                      "results.user.phone", 
+                      "results.user.picture.large", 
+                      "results.user.picture.medium", 
+                      "results.user.picture.thumbnail", 
+                      "results.user.registered",
+                      "results.user.salt",
+                      "results.user.sha1",
+                      "results.user.sha256",
+                      "results.user.username"
+                      
+                     
+    
+    )
+    
+    flatdf.show()
+    
+
+    
     
     
   }
