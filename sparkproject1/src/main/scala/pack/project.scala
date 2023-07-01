d8215a282cf549137f691d67b9b79bd1bebe640f
aatishsingh998 <137929099+aatishsingh998@users.noreply.github.com>
2023-07-01 17:57:41 +0530

    Add files via upload
    Added a project on github

---
+++ sparkproject1/src/main/scala/pack/project.scala
@@ -0,0 +1,26 @@
+package pack
+
+import org.apache.spark.sql.SparkSession
+
+
+object project {
+  def main(args: Array[String]): Unit = {
+    
+    println("spark project started")
+    
+    val spark =SparkSession.builder().appName("project")
+                                     .master("local[*]")
+                                     .getOrCreate()
+   
+    val sc =spark.sparkContext
+    
+    sc.setLogLevel("Error")
+    
+    import spark.implicits._ 
+    
+    println("test")
+    
+    
+    
+  }
+}
